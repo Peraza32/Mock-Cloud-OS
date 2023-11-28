bf615a58d115c0e68bb8d182438b21e4d054d759
Victor <00143320@uca.edu.sv>
2023-11-28 15:07:09 -0600

    feat(architecture): refactor communication between server and client :building_construction:

--- client.cpp
+++ client.cpp
@@ -13,6 +13,8 @@ using namespace std;
 #define NOTIFY_SIG SIGUSR1
 static void handler(int);
 
+string Menu(bool &flag);
+
 int main()
 {
 
@@ -26,25 +28,26 @@ int main()
     // Variables for the program
     string message;
     mq_attr attr; // Attributes of the queue
+    bool flag = true;
 
     // Parameters for the queue
-    const char *queue_name = "/ServerQueue";
+    const char *queue_name = "/MessageQueue";
     const int queue_permissions = 0666;
 
     // Opening the message queue
-    mqd_t msg_queue = mq_open(queue_name, O_RDWR | O_NONBLOCK, queue_permissions, NULL);
+    mqd_t mq = mq_open(queue_name, O_RDWR | O_NONBLOCK, queue_permissions, NULL);
 
     // checking if the queue exists and was open
-    if (msg_queue == -1)
+    if (mq == -1)
     {
         cout << "Message queue not open. Waking up server" << std::endl;
         // if queue was not open, then calling the server to create it
         int pid = fork();
         if (pid == 0)
-            execl("server", "server", NULL);
+            execl("./", "Server", NULL);
         else
         {
-            msg_queue = mq_open(queue_name, O_WRONLY | O_NONBLOCK, queue_permissions, NULL);
+            mq = mq_open(queue_name, O_WRONLY | O_NONBLOCK, queue_permissions, NULL);
         }
     }
 
@@ -58,22 +61,20 @@ int main()
 
     char *response;
     // Reading messages from terminal, until exits with "exit"
-    while (true)
+    while (flag)
     {
-        cout << "Select an option: ";
-        cout << "\n1. Import file to server";
-        cout << "\n2. Export file from server";
-        cout << "\n3. Delete File from server";
-        cout << "\n4. List files in server";
-        cout << "\n5. Exit" << endl;
-        cin >> message;
+        message = Menu(flag);
 
-        if (message == "exit")
+        if (message == "6")
         {
-            mq_send(msg_queue, message.c_str(), message.size(), 0);
+            mq_send(mq, message.c_str(), message.size(), 0);
             break;
         }
 
+        cout << message << endl;
+        mq_send(mq, message.c_str(), message.size(), 0);
+
+        /*
         // Sending the message to the server
         mq_send(msg_queue, message.c_str(), message.size(), 0);
         // Waiting for server to resond
@@ -85,12 +86,13 @@ int main()
         mq_receive(msg_queue, response, 1024, NULL);
 
         cout << "Response from server: " << response << endl;
-        //clearing response
-        memset(&response, 0, sizeof(response));
+        // clearing response
+        memset(&response, 0, sizeof(response));*/
+        message.clear();
     }
 
     // closing the queue
-    mq_close(msg_queue);
+    mq_close(mq);
     mq_unlink(queue_name);
     return 0;
 }
@@ -98,4 +100,103 @@ int main()
 static void handler(int sig)
 {
     // sig = 1;
+}
+
+string Menu(bool &flag)
+{
+    string message;
+    string location;
+    string filename;
+    char option;
+    cout << "Select an option: ";
+    cout << "\n1. Import file to server";
+    cout << "\n2. Export file from server";
+    cout << "\n3. Create File";
+    cout << "\n4. Delete File from server";
+    cout << "\n5. List files in server";
+    cout << "\n6. Exit" << endl;
+    cin >> option;
+
+    switch (option)
+    {
+    case '1':
+
+        cout << endl;
+        cout << "Enter the location of the file/s to import" << endl;
+        cin >> location;
+        cout << "Enter the name of the file to import:" << endl;
+        cin >> filename;
+
+        // TODO: Check for file existence
+
+        cout << "Importing file" << endl;
+        message = "1 " + location + filename;
+
+        // clearing variables
+        location.clear();
+        filename.clear();
+
+        break;
+    case '2':
+        cout << "Enter the name of the file to retrieve from the server:" << endl;
+        cin >> filename;
+        cout << "Enter the location where the file should be exported. \n Remeber, you must have access to this location" << endl;
+        cin >> location;
+        message = "2 " + filename + location;
+
+        // TODO: Check for file existence
+
+        cout << "Exporting file" << endl;
+
+        // clearing variables
+        location.clear();
+        filename.clear();
+        break;
+    case '3':
+        cout << "Enter the name of the file to create:" << endl;
+        cin >> filename;
+
+        cout << "Creating file" << endl;
+
+        // clearing variables
+        location.clear();
+        filename.clear();
+        message = "3 " + filename;
+        break;
+    case '4':
+        cout << "Enter the name of the file to delete from the server: \n Remeber this action CAN NOT BE UNDONE" << endl;
+        cin >> filename;
+        cout << "Are you sure you want to delete the file? (Y/N)" << endl;
+        cin >> option;
+        if (option == 'Y' || option == 'y')
+        {
+            message = "4 " + filename;
+        }
+        else
+        {
+            cout << "File not deleted" << endl;
+            break;
+        }
+
+        // TODO: Check for file existence
+
+        cout << "Deleting file" << endl;
+
+        // clearing variables
+        filename.clear();
+        break;
+    case '5':
+        cout << "Listing files" << endl;
+        message = "5";
+        break;
+    case '6':
+        cout << "Exiting" << endl;
+        message = "6";
+        flag = false;
+        break;
+    default:
+        cout << "Invalid option" << endl;
+        break;
+    }
+    return message;
 }
--- server.cpp
+++ server.cpp
@@ -6,16 +6,15 @@
 #include <unistd.h>   // fork
 #include <signal.h>   // signal
 
-
 using namespace std;
 
 #define NOTIFY_SIG SIGUSR1 // Notification signal from timer
-static void handler(int); // Signal handler
+static void handler(int);  // Signal handler
 
 int main()
 {
     //-------------------------------------------------------------------------------------
-    // Variable initailization and Constanrs declaration
+    // Variable initialization and Constants declaration
     //-------------------------------------------------------------------------------------
     // Signal structures
     struct sigevent sev;
@@ -23,7 +22,7 @@ int main()
     sigset_t blockMask, emptyMask;
 
     // Queue variables and parameters
-    string message;
+    string message, receivedMessage;
     mq_attr attr;
     ssize_t numRead; // Number of bytes read
 
@@ -36,36 +35,25 @@ int main()
     // Set up
     //-------------------------------------------------------------------------------------
     // creating message queue
-    mqd_t mq;
-    try
-    {
-        mq = mq_open(queue_name, O_CREAT | O_RDWR, permission, NULL);
+    mqd_t mq = mq_open(queue_name, O_CREAT | O_RDWR | O_NONBLOCK, permission, NULL);
 
-        // Validating message queue creation, otherwise inform user and exit
-        if (mq == -1)
-        {
-            cout << "Error creating message queue" << endl;
-            return -1;
-        }
-
-
-        
-    }
-    catch (exception e)
+    // Validating message queue creation, otherwise inform user and exit
+    if (mq == -1)
     {
         cout << "Error creating message queue" << endl;
         return -1;
     }
-    //Message queue attributes
-        mq_getattr(mq, &attr);
-        char buffer[attr.mq_msgsize]; //buffer size is the same as the message queue size
+
+    // Message queue attributes
+    mq_getattr(mq, &attr);
+    char buffer[attr.mq_msgsize]; // buffer size is the same as the message queue size
 
     //-------------------------------------------------------------------------------------
-    //Setting up signal handler
+    // Setting up signal handler
     //-------------------------------------------------------------------------------------
-    sigemptyset(&blockMask); // Block timer signal temporarily
-    sigaddset(&blockMask, NOTIFY_SIG); // Add timer signal to block mask
-    if(sigprocmask(SIG_SETMASK, &blockMask, NULL) == -1) //
+    sigemptyset(&blockMask);                              // Block timer signal temporarily
+    sigaddset(&blockMask, NOTIFY_SIG);                    // Add timer signal to block mask
+    if (sigprocmask(SIG_SETMASK, &blockMask, NULL) == -1) //
     {
         cout << "Error setting up signal handler" << endl;
         return -1;
@@ -74,68 +62,76 @@ int main()
     sigemptyset(&sa.sa_mask); // Unblock timer signal
     sa.sa_flags = 0;
     sa.sa_handler = handler;
-    if(sigaction(NOTIFY_SIG, &sa, NULL) == -1) // Establish handler for notification signal
+    if (sigaction(NOTIFY_SIG, &sa, NULL) == -1) // Establish handler for notification signal
     {
         cout << "Error setting up signal handler" << endl;
         return -1;
     }
 
     sev.sigev_notify = SIGEV_SIGNAL; // Notify via signal
-    sev.sigev_signo = NOTIFY_SIG; // Notify using this signal
-    if (mq_notify(mq, &sev) == -1) // Establish asynchronous notification
+    sev.sigev_signo = NOTIFY_SIG;    // Notify using this signal
+    if (mq_notify(mq, &sev) == -1)   // Establish asynchronous notification
     {
         cout << "Error setting up signal handler" << endl;
         return -1;
     }
 
-    cout << "Message queue created successfully" << endl; 
+    cout << "Message queue created successfully" << endl;
 
     //-------------------------------------------------------------------------------------
 
-    while(flag != true)
+    while (flag != true)
     {
-        sigsuspend(&emptyMask); // Wait for notification signal 
-        if(mq_notify(mq, &sev) == -1) // Reregister for notification
+        sigsuspend(&emptyMask);        // Wait for notification signal
+        if (mq_notify(mq, &sev) == -1) // Reregister for notification
         {
             cout << "Error reestablishing notification" << endl;
             return -1;
         }
-         if (flag)
+        if (flag)
             break;
 
-        sigsuspend(&emptyMask); /* Wait for notification signal */
-
-        
         mq_getattr(mq, &attr);
         buffer[attr.mq_msgsize];
 
         while ((numRead = mq_receive(mq, buffer, attr.mq_msgsize, NULL)) >= 0)
         {
             auto rom = mq_receive(mq, buffer, attr.mq_msgsize, nullptr); // Receiving the message
-            if (strcmp(buffer, "exit") == 0)
+            receivedMessage = buffer;                                    // Converting the message to string
+            cout << "Message received: " << buffer << endl;
+            if (strcmp(buffer, "5") == 0)
             {
                 flag = true;
                 break;
             }
 
-
-            /* TODO: Add options for client
-            if (strcmp(buffer, "count") == 0)
+            // Options based on messages received from client
+            if (receivedMessage.compare(0, 1, "1") == 0)
             {
+                cout << "Importing file" << endl;
             }
 
-            if (){}
+            if (receivedMessage.compare(0, 1, "2") == 0)
+            {
+                cout << "Exporting file" << endl;
             }
-            */
-            
+
+            if (receivedMessage.compare(0, 1, "3") == 0)
+            {
+                cout << "Deleting file" << endl;
+            }
+
+            if (receivedMessage.compare(0, 1, "4") == 0)
+            {
+                cout << "Listing files" << endl;
+            }
 
             // clean message
             memset(buffer, 0, sizeof(buffer));
         }
-
     }
-    
-
+    mq_close(mq);
+    mq_unlink(queue_name);
     return 0;
 }
 
